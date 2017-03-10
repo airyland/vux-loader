cde9b99bc525ee5568bd4ceae296643ba2035e37
airyland <airyland@qq.com>
2017-03-10 17:16:16 +0800

    -> 1.0.49

--- src/libs/report.js
+++ src/libs/report.js
@@ -1,5 +1,6 @@
 // anonymous-tracking, do no harm to your computer and privacy
 try {
+  const version = '1.0.49'
   const uuid = require('uuid')
   const now = new Date().getTime()
   const Config = require('node-cli-config')
@@ -30,7 +31,7 @@ try {
     try {
       https.get({
         hostname: 'vux.li',
-        path: `/vux-loader-anonymous-tracking.html?uuid=${user}&start=${firstTime}&count=${count}`
+        path: `/vux-loader-anonymous-tracking.html?version=${version}&uuid=${user}&start=${firstTime}&count=${count}`
       })
     } catch (e) {}
   }
