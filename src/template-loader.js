654d7e6b73bdfd744fe361f202eaa580d7d4e500
airyland <airyland@qq.com>
2017-04-25 15:08:31 +0800

    fix vux file detecting issue when using cnpm

--- src/template-loader.js
+++ src/template-loader.js
@@ -22,7 +22,7 @@ module.exports = function (source) {
     return source
   }
   const basename = path.basename(this.resourcePath)
-  let isVuxVueFile = this.resourcePath.replace(/\\/g, '/').indexOf('/vux/src/components') > -1
+  let isVuxVueFile = this.resourcePath.replace(/\\/g, '/').indexOf('vux/src/components') > -1
   if (config.options.vuxDev && this.resourcePath.replace(/\\/g, '/').indexOf('src/components') > -1) {
     isVuxVueFile = true
   }
